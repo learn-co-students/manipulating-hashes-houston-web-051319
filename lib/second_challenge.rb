4b657513e5b085b3d177d97ba64c6c0b95bb760f
Hans Krohn <hanskrohn70@gmail.com>
2019-05-05 05:41:27 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,13 @@ def second_challenge
   }
 
   #code your solution here!
+  oneDimensionalArr = []
+  multiDimensionArray = groceries.values
 
-  
-
+  multiDimensionArray.each do |subArr|
+    subArr.each do |element|
+      oneDimensionalArr.push(element)
+    end
+  end
+  oneDimensionalArr
 end
